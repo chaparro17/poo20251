6a240fda7a092abf10310be6ad8af6f25df834a5
chaparro17 <santiago.chaparro@campusucc.edu.co>
2025-03-21 00:23:43 +0000

    creacion de la clase operario

--- Clase 20_03_25/Trabajador.java
+++ Clase 20_03_25/Trabajador.java
@@ -1,32 +1,39 @@
 public class Trabajador {
-    // atributos
+ 
+    // Atributos
     private int idTrabajador;
     private String nombre;
     private String apellido;
-
-    // constructor
+ 
+    // Constructor
     public Trabajador(int idTrabajador, String nombre, String apellido) {
-
         this.idTrabajador = idTrabajador;
         this.nombre = nombre;
         this.apellido = apellido;
     }
-
-    // metodos getter
-    public String getNombre() {
-        return nombre;
-    }
-
+ 
+    // métodos getter
     public int getIdTrabajador() {
         return idTrabajador;
     }
-
+ 
+    public String getNombre() {
+        return nombre;
+    }
+ 
+    public String getApellido() {
+        return apellido;
+    }
+ 
+ 
     public double pagar(){
         return 0.0;
     }
-
+ 
     public String toString() {
-        return "Trabajador { idTrabajador:" + idTrabajador + "Nombre: " + nombre + " Apellido: " + apellido + "}";
-
+        return "Trabajador { idTrabajador:" + idTrabajador +
+                " Nombre: " + nombre +
+                " Apellido: " + apellido + "}";
     }
+ 
 }
