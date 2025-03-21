dbe1238fc341b4eceec339420a9d57736ef6bebe
chaparro17 <santiago.chaparro@campusucc.edu.co>
2025-03-21 01:03:56 +0000

    uso del arreglo de objetos

--- Clase 20_03_25/EjecutarHerencia.java
+++ Clase 20_03_25/EjecutarHerencia.java
@@ -1,12 +1,19 @@
 
 public class EjecutarHerencia {
+
     public static void main(String[] args) {
 
-        // Creacion de los objetos (instanciar)
-        Trabajador objT1 = new Operario(1, "Pepito", "puertas", 135.0);
-        Trabajador objT2 = new Operario(2, "Juan", "Puertas", 10);
+        // Arreglo de trabajadores
+        Trabajador[] t = new Trabajador[3];
 
-        System.out.println(objT1);
-        System.out.println(objT2);
+        // Creación de los objetos (Instanciar)
+        t[0] = new Operario(1, "Pepito", "Puertas", 135.0);
+        t[1] = new Consultor(2, "Juan", "Islas", 10);
+        t[2] = new Consultor(3, "Byron", "Cano", 20);
+
+        for (Trabajador x : t) {
+            System.out.println(x.getClass() + " - " + x.pagar());
+        }
+
     }
 }
